795dbb78a32a2b02d2814fcb3f1bc5f49cd7d73a
achyutduggal <achyut1234duggal@gmail.com>
2024-11-24 21:28:25 +0530

    Initial Commit 3

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 # Hugging Face API endpoint and headers
 API_URL = "https://api-inference.huggingface.co/models/nateraw/food"
